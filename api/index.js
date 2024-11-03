9f79e09798ff2044b973c274337c6f7550cd10dd
qhyabdoel <qhyabdoel@gmail.com>
2024-11-03 09:27:56 +0700

    update 2

---
+++ api/index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+const metricsRouter = require("./routes/metrics");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+// parse request body
+app.use(bodyParser.json());
+
+app.use("/", indexRouter);
+app.use("/metrics", metricsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
